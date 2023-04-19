056822e7c34b812dc909c5b70118df07018d5e9b
Daniel Figucio <745341+affrae@users.noreply.github.com>
2023-04-20 08:58:12 +1000

    updates

--- python/PrimeTestCase.py
+++ python/PrimeTestCase.py
@@ -1,7 +1,7 @@
 import unittest
 
 class PrimeTestCase(unittest.TestCase):
-    def test true if is prime (self):
+    def test_true_if_is_prime(self):
         # Test true for all valid primes between 1 and 20
         primes = [2, 3, 5, 7, 11, 13, 17, 19]
         for prime in primes:
@@ -10,16 +10,16 @@ class PrimeTestCase(unittest.TestCase):
     def test_false_if_not_prime(self):
         # Test false for all valid non-primes between 1 and 20
         non_primes = [1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20]
-        for non prime in non primes:
-            self.assertFalse(is_ prime (non prime))
+        for non_prime in non_primes:
+            self.assertFalse(is_prime(non_prime))
 
-    def is prime(number):
-        if number < 2:
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
             return False
-        for i in range(2, number):
-            if number % i == 0:
-                return False
-        return True
+    return True
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     unittest.main()
---
+++ python/utils.py
@@ -0,0 +1,16 @@
+import re
+
+e_regex = r'^[\w\. \+\-]+@[a-zA-Z0-9\-]+\. [a-zA-Z0-91-1.]+$'
+p_regex = r'^(\+) ?1?\d{9,15}$'
+s_regex = r'^(?=.*[a-z]) (?=.*[A-Z]) (?=.*\d) (?=.*[@$!%*?&]) [A-Za-z\d@$!%*?&]{8, }$'
+
+def checkvalid(text, regex):
+    if re.search (regex, text):
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    print(("valid", "invalid") [checkvalid('',e_regex)])
+    print(("valid", "invalid") [checkvalid('',p_regex)])
+    print(("valid", "invalid") [checkvalid('',s_regex)])
