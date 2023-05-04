15b24b51384e69eb1e3e1d3035c76b1e26fa8637
Daniel Figucio <745341+affrae@users.noreply.github.com>
2023-05-05 08:21:14 +1000

    Update PrimeTestCase.py

--- python/PrimeTestCase.py
+++ python/PrimeTestCase.py
@@ -6,12 +6,8 @@ class PrimeTestCase(unittest.TestCase):
         primes = [2, 3, 5, 7, 11, 13, 17, 19]
         for prime in primes:
             self.assertTrue(is_prime(prime))
-    
-    def test_false_if_not_prime(self):
-        # Test false for all valid non-primes between 1 and 20
-        non_primes = [1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20]
-        for non_prime in non_primes:
-            self.assertFalse(is_prime(non_prime))
+
+
 
 def is_prime(number):
     if number < 2:
